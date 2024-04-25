75b379485bce1832afac285d9a04efc566a0cb27
Abhishek gupta <07abhishekgupta07@gmail.com>
2024-04-25 23:50:40 +0530

    Hero banner and Search done(basic structure)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Box } from '@mui/material'
+import Exercises from '../components/Exercises'
+import HeroBanner from '../components/HeroBanner'
+import SeachExercise from '../components/SeachExercise'
 
 function Home() {
   return (
-    <div>Home</div>
+    <Box>
+      <HeroBanner/>
+      <SeachExercise/>
+      <Exercises/>
+    </Box>
   )
 }
 

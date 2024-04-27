85a442a741f39bf25ab016394bb70fbfc32e0fc9
Abhishek gupta <07abhishekgupta07@gmail.com>
2024-04-27 11:37:44 +0530

    recommit

---
+++ src/utils/fetchData.js
@@ -0,0 +1,13 @@
+export const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'cafeb0b203msh98d4311d27ed308p13646ejsn22a29000c595',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
+export const fetchData= async(URL , options)=>{
+    const res = await fetch(URL , options);
+    const data = await res.json();
+    return data;
+}

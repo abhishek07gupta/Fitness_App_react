68c37169c8069f509d5be15834ff2a012e37c217
Abhishek gupta <07abhishekgupta07@gmail.com>
2024-04-29 16:47:49 +0530

    pagination  and exercise loading done

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -5,6 +5,30 @@ import ExerciseCard from './ExerciseCard'
 
 function Exercises({exercises , setExercises , bodyPart}) {
   console.log(exercises)
+  const [currentPage, setCurrentPage] = useState(1)
+  const exercisesPerPage = 5
+  const indexOfLastExercise = currentPage* exercisesPerPage
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
+  const currentExercises = exercises.slice(indexOfFirstExercise,indexOfLastExercise)
+  const paginate=(e,value)=>{
+    setCurrentPage(value);
+
+    window.scroll({top:1800 , behavior: 'smooth'})
+  }
+  useEffect(()=>{
+    const fetchExerciseDatta= async()=>{
+      let exerciseData = []
+
+      if(bodyPart === 'all'){
+        exerciseData = await fetchData('https://exercisedb.p.rapidapi.com/exercises?limit=50', exerciseOptions);
+      }else{
+        exerciseData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}?limit=10`, exerciseOptions);
+
+      }
+      setExercises(exerciseData);
+    }
+    fetchExerciseDatta();
+  },[bodyPart])
   return (
     <Box id="exercises"
     mt='50px'
@@ -17,9 +41,22 @@ function Exercises({exercises , setExercises , bodyPart}) {
       <Stack direction='row' flexWrap='wrap' justifyContent='center'
       sx={{gap:{lg:'110px' , xs:'50px'}}}
       >
-        {exercises.map((exercise,index)=>(
+        {currentExercises.map((exercise,index)=>(
           <ExerciseCard key={index} exercise={exercise}/>
         ))}
+      </Stack>
+      <Stack mt='100px' alignItems='center'>
+        {exercises.length> exercisesPerPage && (
+          <Pagination  
+            color='standard'
+            shape='rounded'
+            defaultPage={1}
+            count={Math.ceil(exercises.length/exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size='large'
+          />
+        )}
       </Stack>
     </Box>
   )
--- src/components/SeachExercise.js
+++ src/components/SeachExercise.js
@@ -19,7 +19,7 @@ const SearchExercises = ({setExercises,setBodyPart,bodyPart}) => {
 
   const handleSearch = async () => {
     if (search) {
-      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises?limit=50', exerciseOptions);
 
       const searchedExercises = exercisesData.filter(
         (item) => item.name.toLowerCase().includes(search)

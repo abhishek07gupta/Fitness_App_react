587f5e1d226db4062c8e1457dd2aa961449eca80
Abhishek gupta <07abhishekgupta07@gmail.com>
2024-04-30 22:27:14 +0530

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Box , Typography , Stack } from '@mui/material'
+import Logo from '../assets/images/Logo-1.png'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <Box mt='80px' bgcolor='#fff3f4'>
+      <Stack gap='40px' alignItems='center' px='40px' pt='24px'>
+        <img src={Logo} alt="logo" width='200px' height='40px' />
+
+        <Typography variant='h5' pb='40px' >
+          Transform your fitness journey with US.
+        </Typography>
+      </Stack>
+    </Box>
   )
 }
 
